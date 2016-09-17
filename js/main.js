7d592d054446ea54d2dd27e939822eca0aa204be
Howard Huang <howardhuang96@gmail.com>
2016-09-17 15:49:09 -0700

    set up handlebars / now retrieves data

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+  //lets get some google spreadsheet data :D
+  $.ajax({
+    dataType: "json",
+    url: "https://spreadsheets.google.com/feeds/list/1Dtx9lL-IA4L_nohyEv9l5CfA5ZM_tHSv5kI7BcWqUJc/od6/public/values?alt=json",
+  	success: function(data) {
+        console.log("got the data");
+  			data = data.feed.entry;
+        console.log(data);
+
+
+        // The code below is for handlebars ---===
+        // Retrieve the template data from the HTML (jQuery is used here).
+
+        // var template = $('#article').html();
+        // var templateScript = Handlebars.compile(template);
+        // var context = data;
+        // var html = templateScript(context);
+        // $("#handlebars-content").append(html);
+        //
+        //
+        // var template2 = $('#tile').html();
+        // var templateScript2 = Handlebars.compile(template2);
+        // var html2 = templateScript2(context);
+        //
+        // $('#tile-container').append(html2);
+
+
+  	}
+  });
+
+});
