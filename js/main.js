7664348aead33b78f03dfc64a5d87a9fb1da8658
Howard Huang <howardhuang96@gmail.com>
2016-09-17 18:43:38 -0700

    saved some changes

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ $( document ).ready(function() {
      asNavFor: '.slider-nav'
   });
   $('.slider-nav').slick({
-     slidesToShow: 1,
+     slidesToShow: 3,
      slidesToScroll: 1,
      asNavFor: '.slider-for',
      dots: true,
