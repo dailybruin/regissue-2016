573562566f96762fe3c0da7d915cb0092312714a
Howard Huang <howardhuang96@gmail.com>
2016-09-18 01:28:15 -0700

    some effects

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         anchors: ['zeroPage', 'firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'eighthPage'],
-        menu: '#menu'
+        menu: '#menu',
+
+        afterLoad: function(anchorLink, index){
+          var loadedSection = $(this);
+          //using anchorLink
+          if(anchorLink != 'zeroPage'){
+              $(".navbar h1").css("color", "white");
+              $(".navbar").css("background-color", "black");
+          }
+          else{
+            $(".navbar h1").css("color", "black");
+            $(".navbar").css("background-color", "transparent");
+          }
+        },
     });
 });
 
