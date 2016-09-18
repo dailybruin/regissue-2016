c75c3981b16153fee913e9413ae6baa2e217732b
Paulina Lei <paulina.y.lei@gmail.com>
2016-09-17 17:57:33 -0700

    i need to go eat dinner now

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        anchors:['firstPage', 'secondPage', 'thirdPage']
+        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage', 'seventhPage', 'eighthPage'],
+        menu: '#menu'
     });
+});
+
+var previousScroll = 0,
+    headerOrgOffset = $('#header').height();
+
+$('#header-wrap').height($('#header').height());
+
+$(window).scroll(function () {
+    var currentScroll = $(this).scrollTop();
+    if (currentScroll > headerOrgOffset) {
+        if (currentScroll > previousScroll) {
+            $('#header-wrap').slideUp();
+        } else {
+            $('#header-wrap').slideDown();
+        }
+    }
+    previousScroll = currentScroll;
 });
